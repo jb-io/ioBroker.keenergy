27441dff1e741740985197e25a3b31d758e56cd2
J.B <anonymous@example.com>
2022-09-26 20:04:43 +0200

    [TASK] Commit first working state of Adapter

--- main.js
+++ main.js
@@ -8,8 +8,11 @@
 // you need to create an adapter
 const utils = require('@iobroker/adapter-core');
 
-// Load your modules here, e.g.:
-// const fs = require("fs");
+// Load your modules here:
+const Keba = require('./lib/Keba');
+const AdapterUtils = require('./lib/AdapterUtils');
+const globalUtils = require('./lib/globalUtils');
+
 
 class Keenergy extends utils.Adapter {
 
@@ -26,66 +29,198 @@ class Keenergy extends utils.Adapter {
         // this.on('objectChange', this.onObjectChange.bind(this));
         // this.on('message', this.onMessage.bind(this));
         this.on('unload', this.onUnload.bind(this));
+
+        this._keba = null;
+    }
+
+    init() {
+        // Initialize your adapter here
+        globalUtils.adapter = this;
+
+        const config = this.config;
+
+        // The adapters config (in the instance object everything under the attribute "native") is accessible via
+        // this.config:
+        // this.log.debug('config host: ' + config.host);
+        // this.log.debug('config updateInterval: ' + config.updateInterval);
+    }
+
+    /**
+     * @returns {Keba}
+     */
+    get keba() {
+        if (!this._keba) {
+            this._keba = new Keba();
+        }
+        return this._keba;
+    }
+
+    /**
+     * @returns {AdapterUtils}
+     */
+    get utils() {
+        if (!this._utils) {
+            this._utils = new AdapterUtils();
+        }
+        return this._utils;
+    }
+
+    /**
+     * @param state {boolean}
+     */
+    setConnectionState(state) {
+        this.setState('info.connection', state, true);
     }
 
     /**
      * Is called when databases are connected and adapter received configuration.
      */
     async onReady() {
-        // Initialize your adapter here
 
         // Reset the connection indicator during startup
-        this.setState('info.connection', false, true);
+        this.setConnectionState(false);
 
-        // The adapters config (in the instance object everything under the attribute "native") is accessible via
-        // this.config:
-        this.log.info('config option1: ' + this.config.option1);
-        this.log.info('config option2: ' + this.config.option2);
+        this.init();
+
+        const keba = this.keba;
+        if (await keba.checkConnection()) {
+            this.setConnectionState(true);
+
+            await keba.collectReadWriteVars();
+
+            for (const name in keba.readWriteVars) {
+                await this.createKebaState(name);
+            }
+
+            this.registerPeriodicalCall();
+        }
 
         /*
         For every state in the system there has to be also an object of type state
         Here a simple template for a boolean variable named "testVariable"
         Because every adapter instance uses its own unique namespace variable names can't collide with other adapters variables
         */
-        await this.setObjectNotExistsAsync('testVariable', {
+
+        // for (const name in readWriteVars) {
+        //     await this.createKebaState(name);
+        // }
+    }
+
+    async createKebaState(name) {
+
+        const item = this.keba.readWriteVars[name];
+        const id = this.utils.transformVariablePath(name);
+
+        const typeMapping = {
+            'numeric': 'number',
+            'bool': 'boolean',
+            'enum': 'string',
+            'text': 'string',
+        }
+
+        let descriptiveName = item.name;
+        if (item.attributes && item.attributes['longText']) {
+            descriptiveName = item.attributes['longText'];
+        }
+        const common = {
+            name: descriptiveName,
+            type: typeMapping[item.type],
+            role: 'value',
+            read: true,
+            write: item.writable,
+        };
+        if (item.unit) {
+            common.unit = item.unit;
+        }
+        if (item.formats) {
+            common.states = {};
+            for (let i = 0; i < item.formats.length; i++) {
+                common.states[i] = item.formats[i] || `UNDEFINED (${i})`;
+            }
+        }
+        if (item.attributes) {
+            if (item.attributes['upperLimit']) {
+                common.max = parseInt(item.attributes['upperLimit']);
+            }
+            if (item.attributes['lowerLimit']) {
+                common.min = parseInt(item.attributes['lowerLimit']);
+            }
+        }
+        await this.setObjectNotExistsAsync(id, {
             type: 'state',
-            common: {
-                name: 'testVariable',
-                type: 'boolean',
-                role: 'indicator',
-                read: true,
-                write: true,
-            },
+            common,
             native: {},
         });
 
-        // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
-        this.subscribeStates('testVariable');
-        // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
-        // this.subscribeStates('lights.*');
-        // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
-        // this.subscribeStates('*');
+        if (item.writable) {
+            // In order to get state updates, you need to subscribe to them. The following line adds a subscription for our variable we have created above.
+            this.subscribeStates(id);
+            // You can also add a subscription for multiple states. The following line watches all states starting with "lights."
+            // this.subscribeStates('lights.*');
+            // Or, if you really must, you can also watch all states. Don't do this if you don't need to. Otherwise this will cause a lot of unnecessary load on the system:
+            // this.subscribeStates('*');
+        }
+    }
 
-        /*
-            setState examples
-            you will notice that each setState will cause the stateChange event to fire (because of above subscribeStates cmd)
-        */
-        // the variable testVariable is set to true as command (ack=false)
-        await this.setStateAsync('testVariable', true);
+    registerPeriodicalCall() {
+        const updateInterval = parseInt(this.config.updateInterval) * 1000;
+        this.periodically = this.setInterval(this.onPeriodically.bind(this), updateInterval);
+    }
 
-        // same thing, but the value is flagged "ack"
-        // ack should be always set to true if the value is received from or acknowledged from the target system
-        await this.setStateAsync('testVariable', { val: true, ack: true });
+    async onPeriodically() {
+        if (await this.keba.checkConnection()) {
 
-        // same thing, but the state is deleted after 30s (getState will return null afterwards)
-        await this.setStateAsync('testVariable', { val: true, ack: true, expire: 30 });
+            const ids = Object.keys(this.keba.readWriteVars);
+            const data = await this.keba.readVars(ids);
 
-        // examples for the checkPassword/checkGroup functions
-        let result = await this.checkPasswordAsync('admin', 'iobroker');
-        this.log.info('check user admin pw iobroker: ' + result);
+            const promises = [];
 
-        result = await this.checkGroupAsync('admin', 'admin');
-        this.log.info('check group user admin group admin: ' + result);
+            for (const item of data) {
+                const itemDefinition = this.keba.readWriteVars[item.name];
+                const val = this.normalizeStateValue(item.value, itemDefinition.type);
+                promises.push(this.setStateAsync(this.utils.transformVariablePath(item.name), { val, ack: true }));
+            }
+
+            await Promise.all(promises);
+        }
+    }
+
+    /**
+     *
+     * @param {string} val
+     * @param {string | null} type
+     * @returns {any}
+     */
+    normalizeStateValue(val, type = null) {
+        switch (type) {
+            case 'numeric':
+            case 'number':
+                return parseFloat(val);
+            case 'boolean':
+            case 'bool':
+                return val === 'true';
+
+        }
+        return val;
+    }
+
+    /**
+     *
+     * @param {any} val
+     * @param {string | null} type
+     * @returns {string}
+     */
+    denormalizeStateValue(val, type = null) {
+        type = type || typeof val;
+        switch (type) {
+            case 'number': {
+                return `${val}`;
+            }
+            case 'boolean': {
+                return val ? 'true' : 'false';
+            }
+        }
+        return val;
     }
 
     /**
@@ -95,10 +230,9 @@ class Keenergy extends utils.Adapter {
     onUnload(callback) {
         try {
             // Here you must clear all timeouts or intervals that may still be active
-            // clearTimeout(timeout1);
-            // clearTimeout(timeout2);
-            // ...
-            // clearInterval(interval1);
+            if (this.periodically) {
+                this.clearInterval(this.periodically);
+            }
 
             callback();
         } catch (e) {
@@ -106,55 +240,56 @@ class Keenergy extends utils.Adapter {
         }
     }
 
-    // If you need to react to object changes, uncomment the following block and the corresponding line in the constructor.
-    // You also need to subscribe to the objects with `this.subscribeObjects`, similar to `this.subscribeStates`.
-    // /**
-    //  * Is called if a subscribed object changes
-    //  * @param {string} id
-    //  * @param {ioBroker.Object | null | undefined} obj
-    //  */
-    // onObjectChange(id, obj) {
-    //     if (obj) {
-    //         // The object was changed
-    //         this.log.info(`object ${id} changed: ${JSON.stringify(obj)}`);
-    //     } else {
-    //         // The object was deleted
-    //         this.log.info(`object ${id} deleted`);
-    //     }
-    // }
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     */
+    async onStateDeleted(id) {
+        const name = this.utils.reverseTransformVariablePath(id);
+
+        this.log.warn(`state ${id} deleted`);
+        this.log.debug(`recreate state ${id} in order to avoid errors.`);
+        await this.createKebaState(name);
+    }
+
+    /**
+     * Is called if a subscribed state changes
+     * @param {string} id
+     * @param {ioBroker.State} state
+     */
+    async onStateChangeExternal(id, state) {
+        const name = this.utils.reverseTransformVariablePath(id);
+        const itemDefinition = this.keba.readWriteVars[name];
+        if (!(itemDefinition && itemDefinition.writable)) {
+            this.log.info(`Ignoring state ${id} changed: ${state.val} (ack = ${state.ack}, by = ${state.from}) because var is not writable.`);
+            return;
+        }
+        this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack}, by = ${state.from})`);
+        await this.keba.writeVar(name, this.denormalizeStateValue(state.val, itemDefinition.type));
+    }
 
     /**
      * Is called if a subscribed state changes
      * @param {string} id
      * @param {ioBroker.State | null | undefined} state
      */
-    onStateChange(id, state) {
-        if (state) {
-            // The state was changed
-            this.log.info(`state ${id} changed: ${state.val} (ack = ${state.ack})`);
-        } else {
+    async onStateChange(id, state) {
+        if (id.search(this.namespace) === 0) {
+            id = id.substring(this.namespace.length + 1);
+        }
+
+        if (!state) {
             // The state was deleted
-            this.log.info(`state ${id} deleted`);
+            await this.onStateDeleted(id);
+            return;
+        }
+        if (!state.ack) {
+            // The state was changed externally
+            await this.onStateChangeExternal(id, state);
         }
     }
 
-    // If you need to accept messages in your adapter, uncomment the following block and the corresponding line in the constructor.
-    // /**
-    //  * Some message was sent to this instance over message box. Used by email, pushover, text2speech, ...
-    //  * Using this method requires "common.messagebox" property to be set to true in io-package.json
-    //  * @param {ioBroker.Message} obj
-    //  */
-    // onMessage(obj) {
-    //     if (typeof obj === 'object' && obj.message) {
-    //         if (obj.command === 'send') {
-    //             // e.g. send email or pushover or whatever
-    //             this.log.info('send command');
 
-    //             // Send response in callback if required
-    //             if (obj.callback) this.sendTo(obj.from, obj.command, 'Message received', obj.callback);
-    //         }
-    //     }
-    // }
 
 }
 
